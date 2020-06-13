857204fda1889bd69ed9b0c8ebc8729942705a37
Andrew Allbright <aallbrig@gmail.com>
2020-06-13 14:05:20 -0400

    User Agent shared with user

--- public_html/index.php
+++ public_html/index.php
@@ -14,6 +14,9 @@
     <title>Daily Couples Journal</title>
   </head>
   <body>
+<?php
+echo "User Agent: " . $_SERVER['HTTP_USER_AGENT'];
+?>
     <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
       <div class="col-md-5 p-lg-5 mx-auto my-5">
         <h1 class="display-4 font-weight-normal">Daily Couples Journal - Through Email!</h1>
