487ac11a1068cb1c077823e251e4c0c84fde5857
Andrew Allbright <aallbrig@gmail.com>
2020-06-13 19:23:15 -0400

    Smaller views break apart index page

---
+++ public_html/views/marketing_pitch.php
@@ -0,0 +1,9 @@
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+    <div class="col-md-5 p-lg-5 mx-auto my-5">
+        <h1 class="display-4 font-weight-normal">Daily Couples Journal - Through Email!</h1>
+        <p class="lead font-weight-normal">Maintain a journal with your partner through email. An email is sent to an alternating partner. Respond to the email.</p>
+        <p class="lead font-weight-normal">At the end of a year, receive all your responses in a PDF book!</p>
+    </div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+</div>
