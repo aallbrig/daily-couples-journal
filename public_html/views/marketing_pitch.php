f2e80db0754cf64f161120e665d3a42fb754f409
Andrew Allbright <aallbrig@gmail.com>
2020-06-13 21:09:20 -0400

    Page reflects text messaging as product instead of email

--- public_html/views/marketing_pitch.php
+++ public_html/views/marketing_pitch.php
@@ -1,8 +1,8 @@
 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
     <div class="col-md-5 p-lg-5 mx-auto my-5">
-        <h1 class="display-4 font-weight-normal">Daily Couples Journal - Through Email!</h1>
-        <p class="lead font-weight-normal">Maintain a journal with your partner through email. An email is sent to an alternating partner. Respond to the email.</p>
-        <p class="lead font-weight-normal">At the end of a year, receive all your responses in a PDF book!</p>
+        <h1 class="display-4 font-weight-normal">Daily Couples Journal - Through Text!</h1>
+        <p class="lead font-weight-normal">Get a text message each day to spur interesting conversations with your partner</p>
+        <p class="lead font-weight-normal">A text message will be sent to both of you to maximize talking each day!</p>
     </div>
     <div class="product-device shadow-sm d-none d-md-block"></div>
     <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
