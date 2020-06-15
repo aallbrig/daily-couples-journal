1c8411e26b6984508166002b4c9a1db2348a61d0
Andrew Allbright <aallbrig@gmail.com>
2020-06-15 19:50:20 -0400

    Google analytics collects site data

--- public_html/views/html_top.php
+++ public_html/views/html_top.php
@@ -1,6 +1,16 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $_ENV["GOOGLE_ANALYTICS_ID"]; ?>"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '<?php echo $_ENV["GOOGLE_ANALYTICS_ID"]; ?>');
+    </script>
+
     <link rel="manifest" href="site.webmanifest">
     <!-- Required meta tags -->
     <meta charset="utf-8">
