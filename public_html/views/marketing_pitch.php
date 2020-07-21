5ea6b5041f9a0e74544934b496afcf406363243a
Andrew Allbright <aallbrig@gmail.com>
2020-07-21 12:52:26 -0400

    Marketing pitch copy improved (slightly)

--- public_html/views/marketing_pitch.php
+++ public_html/views/marketing_pitch.php
@@ -1,8 +1,12 @@
 <div class="position-relative overflow-hidden text-center bg-light">
     <div class="col-md-5 p-lg-5 mx-auto my-5">
-        <h1 class="display-4 font-weight-normal">Daily Couples Journal - Through Text!</h1>
-        <p class="lead font-weight-normal">Get a text message each day to spur interesting conversations with your partner</p>
-        <p class="lead font-weight-normal">A text message will be sent to both of you to maximize talking each day!</p>
+        <h1 class="display-4 mb-5 font-weight-normal">Texts for Couples</h1>
+        <p class="lead font-weight-normal">
+            Sign up to receive a text message each day to prompt an intimate discussion.
+        </p>
+        <p class="lead font-weight-normal">
+            From there, it will be up to the two of you to talk about what we send you!
+        </p>
     </div>
     <div class="product-device shadow-sm d-none d-md-block"></div>
     <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
