a496bedf58f7a73cba43ccec32034356d2b95e07
Andrew Allbright <aallbrig@gmail.com>
2020-08-06 11:46:53 -0400

    Utilize card to make thank you page look nicer

--- public_html/thank-you.php
+++ public_html/thank-you.php
@@ -7,13 +7,22 @@ $tomorrow = date("Y-m-d", strtotime("+1 day"));
 $start_date = $_GET['start_date'] ? $_GET['start_date'] : $tomorrow;
 ?>
     <div class="container">
-        <h2 class="modal-title my-5">Payment Complete!</h2>
-
-        <h5>Next Steps...</h5>
-        <p>Congrats on signing up for this experience!</p>
-        <p>You will receive texts as a couple starting on <b id="start_date"><?php echo $start_date; ?></b>.</p>
-        <p>Once you receive the text for the day, it will be up to the two of you to have the conversation!</p>
-        <p>Remember to have fun! 📱</p>
+        <div class="row">
+            <div class="col">
+                <div class="card shadow my-5">
+                    <div class="card-header text-center">
+                        <h2 class="card-title">Payment Complete!</h2>
+                        <p class="card-subtitle mb-2 text-muted">Congrats on signing up for this experience!</p>
+                    </div>
+                    <div class="card-body">
+                        <h5>Next Steps...</h5>
+                        <p>You will receive texts as a couple starting on <b id="start_date"><?php echo $start_date; ?></b>.</p>
+                        <p>Once you receive the text for the day, it will be up to the two of you to have the conversation!</p>
+                        <p>Remember to have fun! 📱</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 <?php
